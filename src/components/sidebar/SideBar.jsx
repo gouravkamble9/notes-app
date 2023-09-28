8ac071fdb58f03c9aca90b5b68d66bbe4fbe83e2
Gourav <gouravkamble9@gmail.com>
2023-09-28 18:20:33 +0530

    sidebar spin

--- src/components/sidebar/SideBar.jsx
+++ src/components/sidebar/SideBar.jsx
@@ -4,12 +4,17 @@ import "./Sidebar.css";
 const SideBar = (props) => {
 
     const [listOpen,SetListOpen]=useState(false)
+    const [spin,SetSpin]=useState(false)
 
   const colors = ["#fe9b72", "#fec971", "#00d4fe", "#b693fd", "#e4ee91"];
 
   return (
     <div className="sidebar">
-      <img src={add} alt="" onClick={()=>SetListOpen(!listOpen)}/>
+      <img src={add} alt="" 
+      onClick={()=>{
+      SetListOpen(!listOpen) 
+      SetSpin(!spin)}
+      } className={`${spin ? "spin" :" "}`}/>
       <ul className={`sidebar_list ${listOpen ? "sidebar_list_active" : ""}`}>
         {colors.map((item, index) => (
           <li
